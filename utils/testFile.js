92e796f78c882ff0de88a3c1bb186934d6843a4c
silavsale <silavsale@gmail.com>
2020-08-05 18:16:28 -0400

    added eslint

--- utils/testFile.js
+++ utils/testFile.js
@@ -1,22 +1,3 @@
-"use strict";
+const a = 'hello'
 
-function f4() {
-  console.log(this.a);
-}
-
-let obj1 = {
-  a: 1,
-  f4: f4,
-};
-
-let obj2 = {
-  a: 2,
-  obj1: obj1,
-};
-
-let obj3 = {
-  a: 3,
-  f4: obj1.f4,
-};
-
-obj1.f4();
+console.log( `${ a }${ [] }` )
